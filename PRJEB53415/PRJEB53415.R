4144f46631b9ac56cc0cf15129d17912d9d459aa
Rohith <sreesairohith@gmail.com>
2026-07-26 15:34:04 +0530

    Add files via upload

---
+++ PRJEB53415/PRJEB53415.R
@@ -0,0 +1,59 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+table <- args[1]
+taxonomy <- args[2]
+tree <- args[3]
+seqs <- args[4]
+metadata <- args[5]
+
+if (!requireNamespace("remotes", quietly = TRUE)) {
+    install.packages("remotes", repos="https://cloud.r-project.org")
+}
+
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+    install.packages("BiocManager", repos="https://cloud.r-project.org")
+}
+
+if (!requireNamespace("file2meco", quietly = TRUE)) {
+    remotes::install_github("ChiLiubio/file2meco")
+}
+
+if (!requireNamespace("qiime2R", quietly = TRUE)) {
+    remotes::install_github("jbisanz/qiime2R")
+}
+
+if (!requireNamespace("microeco", quietly = TRUE)) {
+    install.packages("microeco", repos="https://cloud.r-project.org")
+}
+
+if (!requireNamespace("phyloseq", quietly = TRUE)) {
+    BiocManager::install("phyloseq", ask = FALSE)
+}
+
+library(file2meco)
+library(microeco)
+library(phyloseq)
+library(qiime2R)
+
+dataset <- qiime2meco(
+  feature_table = table,
+  sample_table = metadata,
+  taxonomy_data = taxonomy,
+  phylo_tree = tree,
+  rep_fasta = seqs
+)
+x <- read_qza(table)
+y <- x$data
+z <- read.csv(metadata)
+rownames(z) <- colnames(y)
+dataset$filter_pollution(taxa = c('mitochondria', 'chloroplast'))
+dataset$tidy_dataset()
+norm <- meco2phyloseq(dataset)
+norm_rar <- rarefy_even_depth(norm, rngseed = T)
+dataset_ps <- tax_glom(norm_rar, taxrank = "Genus")
+otu_table(dataset_ps)
+
+
+final_table <- as.data.frame(otu_table(dataset_ps))
+write.csv(final_table, file = "feature_table.csv",row.names = TRUE)
+
---
+++ PRJEB53415/qiime_manifest.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+from pathlib import Path
+import os
+import argparse
+import pandas as pd
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate manifest to import into qiime, will be generated in the same input directory")
+    parser.add_argument("-r", "--reads_dir", help="Directory containing all the fastq files. (Default: Current directory)", default="./")
+    args=parser.parse_args()
+
+req_dir = Path(args.reads_dir)
+forwardpaths = []
+reversepaths = []
+ids = []
+with open("SRA_Accesion_list_PRJEB36789.txt") as f:
+    for line in f:
+        reqid = line.strip()
+        ids.append(reqid)
+        for file in req_dir.iterdir():
+            if f"{reqid}_1" in file.name:
+                filepath_f = os.path.abspath(file)
+                forwardpaths.append(filepath_f)
+            if f"{reqid}_2" in file.name:
+                filepath_r = os.path.abspath(file)
+                reversepaths.append(filepath_r)
+with open("manifest.tsv", "w") as f:
+    f.write("sample-id\tforward-absolute-filepath\treverse-absolute-filepath\n")
+    for x in ids:
+        for y in forwardpaths:
+            for z in reversepaths:
+                if x in y:
+                    if x in z:
+                        f.write(f"{x}\t{y}\t{z}\n")
+
+
+
