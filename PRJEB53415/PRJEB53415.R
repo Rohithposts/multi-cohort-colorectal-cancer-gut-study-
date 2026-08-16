81a3a33cb2299bb7b75bbc0cb19bab8e59fb0610
Rohith <sreesairohith@gmail.com>
2026-08-16 10:25:37 +0530

    Made ASV ids to genus names in the output abundunce table

--- PRJEB53415/PRJEB53415.R
+++ PRJEB53415/PRJEB53415.R
@@ -37,9 +37,10 @@ dataset$filter_pollution(taxa = c('mitochondria', 'chloroplast'))
 dataset$tidy_dataset()
 norm <- meco2phyloseq(dataset)
 dataset_ps <- tax_glom(norm, taxrank = "Genus")
-otu_table(dataset_ps)
 
 
 final_table <- as.data.frame(otu_table(dataset_ps))
-write.csv(final_table, file = "PRJEB53415_feature_table.csv",row.names = TRUE)
-
+tax_df <- as.data.frame(tax_table(dataset_ps))
+genus_names <- tax_df[rownames(final_table), "Genus"]
+rownames(final_table) <- genus_names
+write.csv(final_table, file = "PRJEB53415_abundunce_table.csv",row.names = TRUE)
